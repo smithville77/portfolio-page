8f82b7607012106b0aa7a78897e386494e6ec5d9
smithville77 <volcom_stone664@hotmail.com>
2024-01-07 21:16:06 +1100

    working on the scroll animation

--- src/js/script.js
+++ src/js/script.js
@@ -6,17 +6,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
   window.addEventListener('scroll', function () {
     const scrollPerc = window.scrollY / (document.documentElement.scrollHeight - containerHeight);
-    const draw = 700 * scrollPerc; 
+    const draw = 700 * scrollPerc;
 
     svgLine.setAttribute("y2", draw + "%");
 
-    
-    if (draw >= 100) {
-      aboutMeSect.style.display = "block";
+    if (draw >= 90) {
+
+        aboutMeSect.style.display = "block"
+
+      if (aboutMeSect.classList.contains("fadeOut")) {
+        aboutMeSect.classList.remove("fadeOut");
+      }
+      aboutMeSect.classList.add("fadeIn");
+    } else if (draw < 90) {
+     
+      aboutMeSect.classList.remove("fadeIn");
+      aboutMeSect.classList.add("fadeOut");
+     
+        aboutMeSect.style.display = "none"
+     
     }
   });
 });
-
 
 
 // document.addEventListener('DOMContentLoaded', function () {
