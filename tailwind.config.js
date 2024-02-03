1cd5afdf9873e531c8dfc18affbd3859133fc511
smithville77 <volcom_stone664@hotmail.com>
2024-02-03 21:02:48 +1100

    few small ui changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,11 @@ module.exports = {
     extend: {
       backgroundColor: {
         bubble: "rgb(17, 63, 55)",
-        card: 'rgb(50, 50, 121)'
+        card: "rgb(50, 50, 121)",
       },
       textColor: {
-        'bubble-text': 'rgb(5, 179, 150)',
+        "bubble-text": "rgb(5, 179, 150)",
       },
-
     },
   },
   plugins: [],
