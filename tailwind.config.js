648afd09ea996a8a830b9bc84534c217d06c1057
Adam Smith <volcom_stone664@hotmail.com>
2024-07-11 22:58:39 +1000

    save

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       backgroundColor: {
         bubble: "rgb(17, 63, 55)",
         new: "rgb(20, 184, 166)",
-        card: "rgb(68, 138, 252)",
+        card: "rgb(177, 207, 255);",
       },
       textColor: {
         "bubble-text": "rgb(5, 179, 150)",
